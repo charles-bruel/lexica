00fa39cbf52c0333e2db872808b5cc2b32818019
charles-bruel <csbruel@gmail.com>
2022-11-06 06:59:56 -0500

    Cleaned up main

--- sound changer/src/main.rs
+++ sound changer/src/main.rs
@@ -16,27 +16,5 @@ pub mod websocket_handler;
 mod tests;
 
 fn main() {
-    // use std::time::Instant;
-    // let now = Instant::now();
-
-    // let prog = constructor::construct(io::load_from_file(&String::from("sava1.csc")).expect(""));
-
-    // let mut words = constructor::construct_words(&prog, io::load_from_file(&String::from("sava-words.txt")).expect(""));
-
-    // words = prog.apply_vec(words);
-
-    // let elapsed = now.elapsed();
-    // print!("Total runtime: {:.2?}\n", elapsed);
-
-    // for w in &words {
-    //     for l in w {
-    //         print!("{}", l.get_symbol(&prog));
-    //     }
-    //     println!();
-    // }
-
-    // let temp = websocket_handler::WebSocketMessage::SaveFile { file_path: String::from("File path"), data: String::from("Data") };
-    // println!("{}", serde_json::to_string(&temp).unwrap());
-
     io::web_socket_listener();
 }
