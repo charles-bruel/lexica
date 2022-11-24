8a8961a1fdddaaba5f24d63fd64afe5f1311e267
charles-bruel <csbruel@gmail.com>
2022-11-23 20:25:00 -0500

    Added function to create the AST Also added some (but not all operators) Function evaluation is still W.I.P.

--- frontend/src/eval_engine.js
+++ frontend/src/eval_engine.js
@@ -4,18 +4,51 @@ var conversions_to_send = {};
 var spreadsheet_references = [];
 var program_test_references = [];
 
-//This AST ultimately evaluates to a single node which can be evaluated or placed into a parent AST
-function create_ast(input) {
+//This will evaluate into an array of ASTNodes, with each ASTNode coming from a single section of comma seperated input
+//Note this is context aware; it only splits on the highest level
+//If the output is a single node, the output of this function can be evaluated or placed into another AST tree
+//The function supports an array of outputs for handling function parameters
+function create_AST(input, expect_single=false) {
     //Process:
     //Divide context into values and operators
     //e.g. [<value>, <+>, <value>]
     //Values are space seperated. Quotes and curly braces act as groupers
-    //Functions are interpreated similarly
+    //Functions are interpreted similarly
+
+    //It's worth noting that when finding groups, only the exact type of character is matched.
+    //For example, it would think in the string {(foo}) the contents of the braces is (foo, despite that being mismatched
+    //This is fine because the error will be caught in the next layer
+
+    //Nodes will contain a running tally of the current nodes. When a comma or EOS is encountered, convert the current set of nodes into an ASTNode,
+    //append it to the result, and move on
+    //nodes will be filled with a combination of ASTNodes and Operators.
+    var nodes = [];
+    var results = [];
+    var running_string = "";
+
+    input += ",";//Little hack to ensure the compaction process after every section, in the same place/way
+
     for(var i = 0;i < input.length;i ++) {
-        var nodes = [];
+        //We immediately check to see if the section should end
+        //The section should end if the current character is an operator or if the running_string is an operator (among other conditions)
+        //If the running_string is an operator, we must deal with that immediately before dealing with grouping or anything else
+        if(check_end_of_symbol(running_string, input[i])) {
+            if(running_string.trim() == "") {
+                //Couple of options here. Either it just finished with a group/quote, in which case whitespace is normal or there are multiple whitespace characters
+                //Either way, the correct thing is to do nothing
+            } else {
+                //Whatever has been accumulating is finished, so figure out what it is and add it
+                nodes.push(get_AST_node(running_string.trim()));
+                running_string = "";
+                //We handled the end of the previous running string intead of the current character, so we have to go back
+                //This will never loop because the above condition of an empty running string will cause a fall through
+                i--;
+            }
+        } else
         //Test for grouping
         if(input[i] == "{") {
-            //Find the end of the grouping section.
+            //Find the end of the grouping section
+            let flag = false;
             let depth = 1;//One curly brace in
             for(var j = i + 1;j < input.length;j ++) {
                 if(input[j] == "{") {
@@ -29,30 +62,192 @@ function create_ast(input) {
                     //Begin is inclusive, so ignore the opening curly brace
                     //End is exclusive, so no need to modify
                     var inner_contents = input.substring(i + 1, j);
-                    nodes.push(create_ast(inner_contents));
+                    nodes.push(create_AST(inner_contents, true));
                     //Rebase i to after the group. i is now on the end curly brace, when i++ is called, it will be the
                     //character after the group ends
                     i = j;
+                    flag = true;
                     break;
                 }
             }
-        }
-        if(input[i] == "\"") {
+            if(!flag) return "ERROR: group parameter section never finishes";
+        } else
+        //Test for function parameters
+        //In this case the contents of running_string is the function name
+        if(input[i] == "(") {
+            //Find the end of the parentheses section
+            let flag = false;
+            let depth = 1;//One paren in
             for(var j = i + 1;j < input.length;j ++) {
-                if(input[i] == "\"") {
+                if(input[j] == "(") {
+                    depth++;
+                }
+                if(input[j] == ")") {
+                    depth--;
+                }
+                if(depth == 0) {
+                    //Get the parameter array as an AST[]
+                    //Begin is inclusive, so ignore the opening curly brace
+                    //End is exclusive, so no need to modify
+                    var inner_contents = input.substring(i + 1, j);
+                    var params = create_AST(inner_contents);
+                    nodes.push(new ASTFunctionNode(running_string, params));
+                    running_string = "";
+                    //Rebase i to after the group. i is now on the end curly brace, when i++ is called, it will be the
+                    //character after the group ends
+                    i = j;
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) return "ERROR: Function parameter section never finishes";
+        } else
+        //Test for string literals
+        if(input[i] == "\"" || input[i] == "\'") {
+            var to_match = input[i];
+            for(var j = i + 1;j < input.length;j ++) {
+                if(input[j] == to_match) {
                     //Check if it has been escaped
+                    //TODO: This does fail if the string ends with ...\\", it will incorrect treat the quote as escaped even though the backslash is escaped
                     if(input[j-1] != "\\") {
                         //Actual end of quote
                         //Add the contents of the quotes as a literal
-                        //Begin is inclusive, so ignore the opening curly brace
+                        //Begin is inclusive, so ignore the opening quote
                         //End is exclusive, so no need to modify
                         var inner_contents = input.substring(i + 1, j);
-                        nodes.push(create_string_literal_ast_node(inner_contents));
+                        nodes.push(new ASTStringLiteralNode(inner_contents));
+                        //Rebase i to after the group. i is now on the end quote, when i++ is called, it will be the
+                        //character after the group ends
+                        i = j;
+                        break;
                     }
                 }
             }
+        } else
+        //Finally do the accumulation
+        if(input[i] != ",") {
+            //No whitespace or anything else; continue as normal
+            //This is placed here so the compaction section can go in it's natural place at the end
+            running_string += input[i];
+        } else {
+            //Implicit if(input[i] == ",")
+            
+            //nodes is now filled with ASTNodes and Operators
+            //This section will then compact Operators and ASTNodes into ASTNode(s)
+            //Parentheses will already be taken care of by this point
+            //All literals/immediate values should already be in ASTNodes
+            
+            //Process:
+            //First apply unary operators
+            //Then, going left to right, apply binary operators
+            //nodes will be used as working space
+            //Apply unary operators
+            for(var j = 0;j < nodes.length;j ++) {
+                if(nodes[j] instanceof ASTUnaryOperator) {
+                    if(j == nodes.length - 1) {
+                        //The unary operator is the final node, therefore it has no operand
+                        return "ERROR: Hanging unary operator"
+                    }
+                    nodes[j] = nodes[j].get_node(nodes[j + 1]);//Replace the operator with a node with the operand reference
+                    nodes.splice(j + 1, 1);//Remove the j + 1th item from the array
+                    //j will automatically be place on the next item, no need to rebase
+                }
+            }
+
+            //Apply binary operators
+            for(var j = 0;j < nodes.length;j ++) {
+                if(nodes[j] instanceof ASTBinaryOperator) {
+                    if(j == 0 || j == nodes.length - 1) {
+                        //The binary operator is the final node or it is the first node, therefore it missing an operand
+                        return "ERROR: Hanging binary operator"
+                    }
+                    nodes[j - 1] = nodes[j].get_node(nodes[j - 1], nodes[j + 1]);//Replace the operator with a node with the operand reference
+                    nodes.splice(j, 2);//Remove the jth and j + 1th item from the array
+                    //Removes the original operator and the following value, leaving the node where the first operand was
+                    //j is will have now skipped an item, so go 1 back
+                    j--;
+                }
+            }
+
+            if(nodes.length == 0) {
+                return "ERROR: Blank input";
+            }
+            if(nodes.length > 1) {
+                console.log(nodes);
+                return "ERROR: When collapsing, was left with multiple sections";
+            }
+
+            results.push(nodes[0]);//Save result
+            nodes = [];//Reset nodes for next parameter
         }
     }
+
+    //If we expect a single result, return a single result for convience
+    //Also allows some error checking
+    //TODO: Maybe find a better error checking approach
+    //I really miss rust results...
+    if(expect_single) {
+        if(results.length == 0) {
+            return "ERROR: Empty group"
+        } else if(results.length == 1) {
+            return results[0];
+        } else {
+            return "ERROR: Expected single node, got " + results.length;
+        }
+    }
+
+    return results;
+}
+
+//This function checks if the symbol should end now
+//This detects stuff like "5+4", with just checking whitespace, the system would falsely interpret that as a single literal
+//Not having to do an ugly hack like this is the advantage of using a tokenizer before creating an AST
+//However, given the simplicity of the language, this is fine but is a potential point for improvement
+function check_end_of_symbol(running_string, to_add) {
+    if(/\s/.test(to_add)) return true;
+    if(to_add == "," && running_string != "") return true;
+    for(const x of ASTUnaryOperator.operators) {
+        if(x === to_add && running_string != "") return true;
+        if(x === running_string) return true;
+    }
+    for(const x of ASTBinaryOperator.operators) {
+        if(x === to_add && running_string != "") return true;
+        if(x === running_string) return true;
+    }
+    return false;
+}
+
+//This turns a given input into an AST node
+//The name is actually slightly incorrect, as it might return an operator or an AST node
+//Grouping, functions, and string literals are assumed to have been dealt with; this just deals with operators and literals
+function get_AST_node(input) {
+    //Process:
+    //First check if it is an operator
+    //Then try to convert it into a literal
+    
+    for(const x of ASTUnaryOperator.operators) {
+        if(x === input) {
+            return new ASTUnaryOperator(x);
+        }
+    }
+
+    for(const x of ASTBinaryOperator.operators) {
+        if(x === input) {
+            return new ASTBinaryOperator(x);
+        }
+    }
+
+    //TODO: Implement other literal types
+    //Perhaps programs should be literals?
+    //For now, we assume all remaining literals are numbers
+    //Recall strings are already captured
+    var result = parseFloat(input);
+    if(!isNaN(result)) {
+        //Sucessfully parsed numeric literal
+        return new ASTNumericLiteralNode(result);
+    }
+
+    return "ERROR: Could not parse literal \"" + input + "\"";
 }
 
 function eval_value(input, pos) {
--- frontend/src/eval_engine_helpers.js
+++ frontend/src/eval_engine_helpers.js
@@ -69,3 +69,147 @@ function handle_sc_response(entries) {
         }
     }
 }
+
+function manage_escaped_characters(input) {
+    var result = "";
+    for(var i = 0;i < input.length;i ++) {
+        //Detect the beggining of escapes
+        if(input == "\\") {
+            if(i == input.length - 1) {
+                //This *should* be unreachable
+                //For their to be a backslash here, the last character would need to be a backslash, escaping the end quotes.
+                //If the backslash is escaped by a backslash before it, this backslash would not be processsed
+            } else {
+                if(input[i + 1] == "n") {
+                    result += "\n";//Add \n support
+                } else {
+                    //We don't support most of the javascript special escapes, and all the remaining ones (\", \', \\) just copy the character.
+                    //It should be UB to escape anything else, but just keeping it as-is should be simpler.
+                    result += input[i+1];
+                }
+            }
+        } else {
+            result += input[i];
+        }
+    }
+    return result;
+}
+
+//AST nodes
+class ASTNode {}
+
+class ASTStringLiteralNode extends ASTNode {
+    constructor(string) {
+        super();
+        this.literal = manage_escaped_characters(string);
+    }
+    evaluate() {
+        return this.literal;
+    }
+    avaliable() {
+        return true;
+    }
+}
+
+class ASTNumericLiteralNode extends ASTNode {
+    constructor(value) {
+        super();
+        this.literal = value;
+    }
+    evaluate() {
+        return this.literal;
+    }
+    avaliable() {
+        return true;
+    }
+}
+
+class ASTFunctionNode extends ASTNode {
+    constructor(function_name, params) {
+        super();
+        this.function_name = function_name;
+        this.params = params;
+    }
+    evaluate() {
+        return "TODO";
+    }
+    avaliable() {
+        return "TODO";
+    }
+}
+
+class ASTUnaryNode extends ASTNode {
+    constructor(operand) {
+        super();
+        this.operand = operand;
+    }
+    avaliable() {
+        return this.operand.avaliable();
+    }
+}
+
+class ASTBinaryNode extends ASTNode {
+    constructor(operand_left, operand_right) {
+        super();
+        this.operand_left = operand_left;
+        this.operand_right = operand_right;
+    }
+    avaliable() {
+        return this.operand_left.avaliable() && this.operand_right.avaliable();
+    }
+}
+
+class ASTOperator {}
+
+function create_AST_unary_node(fn) {
+    var temp = Object.create(ASTUnaryNode);
+    temp.evaluate = fn;
+    return temp;
+}
+
+function create_AST_binary_node(fn) {
+    var temp = Object.create(ASTBinaryNode);
+    temp.evaluate = fn;
+    return temp;
+}
+
+class ASTUnaryOperator extends ASTOperator {
+    //TODO: Find away to use "-" as both a unary and binary operator
+    static operators = ["~", "!"];
+
+    //operator must be one of the above strings
+    constructor(operator) {
+        super();
+        this.operator = operator;
+    }
+
+    get_node(operand) {
+        switch(this.operator) {
+            case "~":
+                return create_AST_unary_node(function() { return -operand.evaluate(); });
+            case "!":
+                return create_AST_unary_node(function() { return operand.evaluate() == 0 ? 1 : 0 });
+        }
+        return "TODO";
+    }
+}
+
+class ASTBinaryOperator extends ASTOperator {
+    static operators = ["+", "-", "*", "/", "==", "!=", ">", "<", ">=", "<="];
+
+    //operator must be one of the above strings
+    constructor(operator) {
+        super();
+        this.operator = operator;
+    }
+
+    get_node(operand_left, operand_right) {
+        switch(this.operator) {
+            case "+":
+                return create_AST_binary_node(function() { return operand_left.evaluate() + operand_right.evaluate(); });
+            case "-":
+                return create_AST_binary_node(function() { return operand_left.evaluate() - operand_right.evaluate(); });
+        }
+        return "TODO";
+    }
+}
