411bb36d8262218c45d0177694ac73ee04bf7969
Charles Bruel <csbruel@gmail.com>
2023-06-14 00:15:11 +0200

    Expansion into full table array

--- sound changer/src/manual_ux/project.rs
+++ sound changer/src/manual_ux/project.rs
@@ -7,10 +7,33 @@ pub struct Project {
 }
 
 pub fn load_project(filepath: String) -> Project {
+    let mut max_id: usize = 0;
     let mut accumulation: Vec<Table> = Vec::new();
     for entry in glob::glob(&(filepath + &String::from("/**/*.ltable"))).unwrap() {
-        accumulation.push(table::load_table(&io::load_from_file(&entry.unwrap().as_path().display().to_string(), false).unwrap()));
+        let temp = table::load_table(&io::load_from_file(&entry.unwrap().as_path().display().to_string(), false).unwrap());
+        if <u16 as Into<usize>>::into(temp.id) > max_id {
+            max_id = temp.id.into();
+        }
+        accumulation.push(temp);
     }
-    println!("{:?}", accumulation);
-    todo!()
+
+    let mut tables = Vec::with_capacity(max_id);
+
+    // Initial population
+    let mut i = 0;
+    while i < max_id {
+        tables.push(Option::None);
+        i += 1;
+    }
+
+    // Population with accumulation
+    for table in accumulation {
+        let id: usize = table.id.into();
+        if tables[id].is_some() {
+            panic!("ID Collision")
+        }
+        tables[id] = Option::Some(table);
+    }
+
+    Project { tables }
 }   
